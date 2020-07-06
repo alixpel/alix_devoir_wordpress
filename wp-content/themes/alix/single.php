d5ad293bed01c46fedcfb8a121e602043bf491e4
Alix Pelletier <alixpelletierpro@gmail.com>
2020-07-06 14:35:25 +0200

    single.php dynamic content except pagination

--- wp-content/themes/alix/single.php
+++ wp-content/themes/alix/single.php
@@ -1,51 +1,44 @@
 <?php get_header(); ?>
   <main class="container site-content">
+    <?php if(have_posts()) : ?>
+      <?php while(have_posts()) : the_post(); ?>
       <article class="entry single">
         <header class="entry-header">
           <section class="entry-metadata">
             <section class="entry-data">
-              <h4 class="author"><a href="#">Ada Lovelace</a></h4>
-              <h6 class="publish-date">29 juin 2020</h6>
-              <h5 class="entry-category"><a href="#">Actualité</a></h5>
-              <h4 class="comments-number"><i class="fas fa-comment"></i> 3 commenatires</h4>
+              <h4 class="author"><a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>">
+                <?php the_author(); ?></a></h4>
+              <h6 class="publish-date"><?php the_time('d M Y'); ?></h6>
+              <?php
+              $categories = get_the_category();
+              $separator = " ";
+              $output = '';
+
+              if($categories) {
+                forEach($categories as $category) {
+                  $output .= '<h5 class="entry-category"><a href="'.get_category_link($category
+                  ->term_id).'">'.$category->cat_name .'</a></h5>' . $separator;
+                }
+              }
+              echo trim($output, $separator);
+              ?>
+              <h4 class="comments-number"><i class="fas fa-comment"></i><?php comments_number('Aucun commentaire', '1 commentaire',
+             '% commentaires'); ?></h4>
             </section>
-            <h2 class="entry-title">Vos meilleurs moments en concert</h2>
+            <h2 class="entry-title"><?php the_title(); ?></h2>
           </section>
-          <img src="./assets/images/joey-thompson-unsplash.jpg" alt="Foule"
-          class="featured-image">
+          <?php
+          if(has_post_thumbnail()) :
+            $thumbnail_id = get_post_thumbnail_id();
+            $thumbnail_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+            $thumbnail_title = get_the_title($thumbnail_id);
+            the_post_thumbnail('full', ['class' => 'featured-image', 'title' => $thumbnail_title,
+            'alt' => $thumbnail_alt]);
+           endif;
+           ?>
         </header>
         <section class="entry-content">
-          <p>
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deseru mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-            do eiusmod tempor incididunt…
-          </p>
-
-          <p>
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-            aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem i
-            psum quia dolor sit amet, consectetur, adipisci velit, sed quia
-            non numquam eius modi tempora incidunt ut labore et dolore magnam
-            aliquam quaerat voluptatem.
-          </p>
-
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo.
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur.
-          </p>
+          <?php the_content(); ?>
         </section>
         <footer class="entry-footer">
           <section class="author-card">
@@ -53,33 +46,26 @@
               <img src="./assets/images/logo-rock-band.png" alt="Photo de l'auteur" class="author-picture">
             </section>
             <section class="author-metadata">
-              <h3 class="author-meta-name">Ada Lovelace</h3>
+              <h3 class="author-meta-name"><?php get_the_author_meta('get_the_author_meta'); ?></h3>
               <p class="author-meta-description">
-                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt.
+                <?php get_the_author_meta('description'); ?>
               </p>
             </section>
           </section>
           <nav class="navigation pagination entry-pagination">
-            <ul>
-              <li><a href="#"><i class="fas fa-arrow-left"></i> Nos 5 meilleurs souvenirs en concert</a></li>
-              <li><a href="#">Comment bien préparer un concert en 3 étapes<i class="fas fa-arrow-right"></i></a></li>
-            </ul>
+            <?php
+          // Structure du tableau d'arguments : https://developer.wordpress.org/reference/functions/get_the_post_navigation/
+          // %title : indique à WP qu'il faut inclure le nom de l'article suivant/ précédent
+          the_post_navigation(
+            array(
+              'prev_text'          => '<i class="fas fa-arrow-left"></i> %title',
+              'next_text'          => '%title <i class="fas fa-arrow-right"></i>'
+            )
+          );
+          ?>
           </nav>
           <section class="comments">
-            <h3 class="comments-title">3 commentaires pour "Vos meilleurs moments en concert"</h3>
-            <p>Ici : liste des commentaires de l'article</p>
-            <h3 class="comments-title">Laisser un commentaire</h3>
-            <form class="comment-form" action="index.html" method="post">
-              <label for="name">Nom</label>
-              <input type="text" name="name" required>
-              <label for="email">E-mail</label>
-              <input type="email" name="email" required>
-              <label for="comment">Commentaire</label>
-              <input type="textarea" rows="10" cols="80" name="comment" required>
-              <input type="submit" name="submit" value="Envoyer">
-            </form>
+            <?php comments_template(); ?>
           </section>
         </footer>
       </article>
